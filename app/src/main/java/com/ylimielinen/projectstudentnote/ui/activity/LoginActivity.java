04cf0c96cf330f124c424c3382bb8d6388dabd6f
kevinberret <berret.kevin@gmail.com>
2017-10-28 12:38:16 +0200

    Activities order (1st login then main) Design modifications

--- app/src/main/java/com/ylimielinen/projectstudentnote/ui/activity/LoginActivity.java
+++ app/src/main/java/com/ylimielinen/projectstudentnote/ui/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.ylimielinen.projectstudentnote.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ylimielinen.projectstudentnote.R;
 
@@ -10,6 +13,20 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__login);
+        setContentView(R.layout.activity_login);
+
+        Button mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    private void attemptLogin(){
+        // Open the main activity
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
