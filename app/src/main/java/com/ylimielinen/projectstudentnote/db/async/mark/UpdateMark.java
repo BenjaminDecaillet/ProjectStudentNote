4dc44d08df0b0b8a5002f83873a28cbad17d4045
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2017-11-12 14:24:51 +0100

    Update Functions

---
+++ app/src/main/java/com/ylimielinen/projectstudentnote/db/async/mark/UpdateMark.java
@@ -0,0 +1,28 @@
+package com.ylimielinen.projectstudentnote.db.async.mark;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.ylimielinen.projectstudentnote.db.DatabaseCreator;
+import com.ylimielinen.projectstudentnote.db.entity.MarkEntity;
+
+/**
+ * Created by decai on 12.11.2017.
+ */
+
+public class UpdateMark extends AsyncTask<MarkEntity, Void, Void> {
+
+    private Context mContext;
+
+    public UpdateMark(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected Void doInBackground(MarkEntity... markEntities) {
+        DatabaseCreator dbCreator = DatabaseCreator.getInstance(mContext);
+        for (MarkEntity mark : markEntities)
+            dbCreator.getDatabase().markDao().update(mark);
+        return null;
+    }
+}
---
+++ app/src/main/java/com/ylimielinen/projectstudentnote/db/async/student/UpdateStudent.java
@@ -0,0 +1,28 @@
+package com.ylimielinen.projectstudentnote.db.async.student;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.ylimielinen.projectstudentnote.db.DatabaseCreator;
+import com.ylimielinen.projectstudentnote.db.entity.StudentEntity;
+
+/**
+ * Created by decai on 12.11.2017.
+ */
+
+public class UpdateStudent extends AsyncTask<StudentEntity, Void, Void> {
+
+    private Context mContext;
+
+    public UpdateStudent(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected Void doInBackground(StudentEntity... studentEntities) {
+        DatabaseCreator dbCreator = DatabaseCreator.getInstance(mContext);
+        for (StudentEntity student : studentEntities)
+            dbCreator.getDatabase().studentDao().update(student);
+        return null;
+    }
+}
